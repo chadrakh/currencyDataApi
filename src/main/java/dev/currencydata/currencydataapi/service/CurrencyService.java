d4c50c59aec9e4970dc445a32f4b0b649d5e82f4
chadrakh <chadrak@outlook.com>
2023-06-05 16:30:33 +0100

    currency service functionality started

--- src/main/java/dev/currencydata/currencydataapi/service/CurrencyService.java
+++ src/main/java/dev/currencydata/currencydataapi/service/CurrencyService.java
@@ -5,7 +5,45 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CurrencyService {
-//    public Currency getCurrency(String currency) {
+    public Currency getCurrency(int id) {
+        return null;
+    }
+
+    public Currency getCurrency(String name) {
+        return null;
+    }
+
+    public Iterable<Currency> getCurrencies() {
+        return null;
+    }
+
+    public Currency getCurrencyCodes() {
+        return null;
+    }
+
+//    public static void mapGamesToDatabase() {
 //
+//        try {
+//            // Establish connection to the H2 database
+//            Connection connection = DriverManager.getConnection("jdbc:h2:file:~/springboot", "", "");
+//
+//            // Read the JSON file
+//            JSONParser jsonParser = new JSONParser();
+//
+//            FileReader currencyJson = new FileReader("src/main/dev/currencydata/currencydataapi/_data/currencies.json");
+//            JSONArray currenciesJsonArray = (JSONArray) jsonParser.parse(currencyJson);
+//
+//            List currenciesArray = new ArrayList<>();
+//
+//            for (int i = 0; i < currencyJson.size(); i++) {
+//                var currency = currencyJson.get(i);
+//                currenciesArray.add(currency);
+//            }
+//
+//            System.out.println(currenciesArray);
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 //    }
 }
